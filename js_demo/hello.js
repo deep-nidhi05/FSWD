caec737e35ca833354e28a367c83b8bf071e5ce3
Deepnidhi Kumari <deepnidhik@gmail.com>
2023-07-11 09:54:06 +0530

    bla

---
+++ js_demo/hello.js
@@ -0,0 +1,15 @@
+//console.log("hello World...!!");
+//var,let,const
+//var a=-10.2356
+//var a=9;
+//let b=0;
+//const pi=3.14;
+//console.log(typeof(a));
+//operators in java script
+//  var a=10;
+//  var b=50;
+//  console.log(a+b);
+
+var head1=<h1>hello</h1>;
+console.log(head1);
+
---
+++ js_demo/scripts.js
@@ -0,0 +1,8 @@
+//document.getElementById("head1").innerText="hello";
+// document.write("hello");
+// Window.alert('this is a basic alert');
+function clickme(){
+    // document.getElementById("para").style.display = "block";
+    // document.getElementById('image1')
+    scrollTo(0,500);
+}
